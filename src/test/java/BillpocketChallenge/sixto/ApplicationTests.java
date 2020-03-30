ae955832994a04df9473eea730d96cd6020c6483
Sixto Venegas <sixto.venegas@bsdenterprise.com>
2020-03-30 11:40:58 -0600

    init commit

---
+++ src/test/java/BillpocketChallenge/sixto/ApplicationTests.java
@@ -0,0 +1,13 @@
+package BillpocketChallenge.sixto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
